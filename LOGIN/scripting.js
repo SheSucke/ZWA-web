26995a92038946fa2ed701a6d76379a9843949f6
David <stepad10@fel.cvut.cz>
2020-01-05 18:33:56 +0100

    first commit

---
+++ LOGIN/scripting.js
@@ -0,0 +1,27 @@
+function hover(){
+    const js = document.querySelectorAll(".ul-butts a");
+    for(var i = 0 ; i < js.length; i++){
+      js[i].addEventListener('mouseover', changeDefOver);
+      js[i].addEventListener('mouseout', changeDefOut);
+    }
+    function changeDefOver(e) {
+      e.target.classList.toggle("js-style");
+      //e.target.style.backgroundColor = "var(--a-hover-color)";
+      //e.target.style.color = "beige";
+    }
+      
+    function changeDefOut(e) {
+      e.target.classList.toggle("js-style");
+      //e.target.style.backgroundColor = "var(--a-color)";
+      //e.target.style.color = "initial";
+    }
+}
+function back(){
+    const element = document.querySelector('#back');
+
+    element.setAttribute('href', document.referrer);
+    element.onclick = function() {
+    history.back();
+    return false;
+    }
+}
